f07c41675d219cfc7f3c3411ce1a9c5c581d4865
Abdullah Mia <abdullah149161@bsdi-bd.org>
2022-08-27 03:55:19 +0600

    [Update profile]

--- src/controllers/userContoller.js
+++ src/controllers/userContoller.js
@@ -11,3 +11,32 @@ module.exports.getUser = async (req, res) => {
         return res.status(500).json({ msg: err.message });
     }
 };
+
+// update profile
+module.exports.updateProfile = async (req, res) => {
+    try {
+        const user = req.user;
+        const body = req.body;
+
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: user.id },
+            body,
+            {
+                new: true,
+            }
+        );
+        await updatedUser.save();
+
+        return res.status(200).json({
+            message: "Profile has been updated",
+            user: {
+                fullName: updatedUser.fullName,
+                username: updatedUser.username,
+                email: updatedUser.email,
+                id: updatedUser._id,
+            },
+        });
+    } catch (err) {
+        return res.status(500).json({ msg: err.message });
+    }
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,9 @@
-const { getUser } = require("../controllers/userContoller");
+const { getUser, updateProfile } = require("../controllers/userContoller");
 const { isLoggedIn } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 router.get("/:username", isLoggedIn, getUser);
+router.patch("/update-profile", isLoggedIn, updateProfile);
 
 module.exports = router;
